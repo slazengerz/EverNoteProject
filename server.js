aef6dd0c8b0a2f1c62ea4f5e7b107379c39d47ef
pankaj <slazengerz@gmail.com>
2019-10-23 06:45:02 +1100

    adding files

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require('express')(); // Create an instance of an Express app
+
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+
+mobileApp.tables.add('NoteBook'); 
+mobileApp.tables.add('Notes'); 
+mobileApp.tables.add('Users'); 
+
+app.use(mobileApp);
+app.listen(process.env.PORT || 3000);
